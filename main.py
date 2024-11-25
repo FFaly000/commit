dfc426110ac438fcaf7f90d72d40500f0ab4c9a2
FFaly000 <164123310+FFaly000@users.noreply.github.com>
2024-11-25 22:37:32 +0000

    h

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 
-commitCount = 1000
-autoPush = "y"
+commit = 1000
+y = "y"
 
 i = 0
 while True:
-    for _ in range(commitCount):
-        os.system(f'git commit --allow-empty -m "Commit {i+1} of {commitCount}"')
+    for _ in range(commit):
+        os.system(f'git commit --allow-empty -m "Commit {i+1} of {commit}"')
         i += 1
 
-    print(f"Committed {commitCount} times. Total commits: {i}")
+    print(f"Committed {commit} times. Total commits: {i}")
 
-    if autoPush == "y":
+    if y == "y":
         os.system('git push')
